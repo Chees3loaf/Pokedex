c918fa64f1ccd58f1934b09d27731eb52196f29f
Chees3loaf <simiyes2@gmail.com>
2023-09-06 22:33:33 -0700

    Evening Update

--- Snorlax/Rest.go
+++ Snorlax/Rest.go
@@ -3,7 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
+	"bufio"
+	"os"
+	"os/exec"
+	
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
@@ -28,10 +33,83 @@ func headers(w http.ResponseWriter, req *http.Request) {
 
 }
 
+func wait(seconds int) {
+	done := make(chan struct{})
+
+	go func() {
+		time.Sleep(time.Duration(seconds) * time.Second)
+		close(done)
+	}()
+
+	<-done
+	fmt.Printf("Waited for %d seconds./n,", seconds)
+
+}
+//Clears screen in cmd
+func Cls() {
+
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+
+}
+
+func StringPrompt(label string) string {
+	var s string
+	r := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Fprint(os.Stderr, label+" ")
+		s, _ = r.ReadString('\n')
+		if s != " " {
+			break
+		}
+	}
+	return strings.TrimSpace(s)
+
+}
+
 func main() {
+	mode := 0 // 0: unset, 1: Eat Me, 2: Drink Me, 3: Wake Up
+	var err error
+	var resp []byte
+	var interfaces, interfaceList []string
+	var adapterNum uint32 = 0
 
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/headers", headers)
 
 	http.ListenAndServe(":8090", nil)
+
+	wait(3)
+
+
+MainMenu:
+	mode = 0
+	fmt.Println("Choose One")
+	fmt.Println("1. Eat Me")
+	fmt.Println("2. Drink Me")
+	fmt.Println("3. Wake Up")
+	res := StringPrompt("One or Two, What shall I do?")
+	if res == "1" {
+		mode = 1
+		goto EatMe
+	} else if res == "2" {
+		mode = 2
+		goto DrinkMe
+	} else if res == "3" {
+		Cls()
+		fmt.Println("Goodbye Friend!")
+		time.Sleep(3 * time.seconds)
+		goto Exit
+	} else {
+		fmt.Println("1, 2 or 3, it's all the same to me...")
+		time.Sleep(3 * time.seconds)
+		goto MainMenu
+	}
+
+EatMe:
+	Cls()
+	
+
+
 }
