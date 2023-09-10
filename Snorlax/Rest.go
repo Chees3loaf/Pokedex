4d3b847eb6ab4305502c7665f4a6de7a096838af
Chees3loaf <simiyes2@gmail.com>
2023-09-09 23:10:59 -0700

    Big update
    Thinned out the API code and began the client side code as well as the client app

--- Snorlax/Rest.go
+++ Snorlax/Rest.go
@@ -3,16 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strings"
 	"time"
-	"bufio"
 	"os"
-	"os/exec"
-	"github.com/Chees3loaf/Pokedex/Snorlax/moves"
+	
 	
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
+	func hello(w http.ResponseWriter, req *http.Request) {
 	//t := now.Local()
 	t := time.Now()
 
@@ -24,116 +21,53 @@ func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(w, "It's", t.Format(time.Stamp))
 }
 
-func headers(w http.ResponseWriter, req *http.Request) {
+	func headers(w http.ResponseWriter, req *http.Request) {
 
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
+		for name, headers := range req.Header {
+			for _, h := range headers {
+				fmt.Fprintf(w, "%v: %v\n", name, h)
+			}
 		}
+
+}
+
+func eatMeHandler(w http.ResponseWriter, req *http.Request) {
+	files, err := os.ReadDir("./EatMe")
+	if err != nil {
+		http.Error(w, "Unable to read EatMe directory", http.StatusInternalServerError)
+		return
 	}
 
-}
-
-func wait(seconds int) {
-	done := make(chan struct{})
-
-	go func() {
-		time.Sleep(time.Duration(seconds) * time.Second)
-		close(done)
-	}()
-
-	<-done
-	//fmt.Printf("Waited for %d seconds./n,", seconds)
-
-}
-//Clears screen in cmd
-func Cls() {
-
-	cmd := exec.Command("cmd", "/c", "cls")
-	cmd.Stdout = os.Stdout
-	cmd.Run()
-
-}
-
-func StringPrompt(label string) string {
-	var s string
-	r := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Fprint(os.Stderr, label+" ")
-		s, _ = r.ReadString('\n')
-		if s != " " {
-			break
-		}
+	fmt.Fprintln(w, "The Finest of Tavern Foods")
+	for _, file := range files {
+		fmt.Fprintln(w, file.Name())
 	}
-	return strings.TrimSpace(s)
-
 }
+
+func drinkMeHandler(w http.ResponseWriter, req *http.Request) {
+	files, err := os.ReadDir("./DrinkMe")
+	if err != nil {
+		http.Error(w, "Unable to read DrinkMe directory", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintln(w, "A Very Marry Un-Birthday to You!")
+	fmt.Fprintln(w, "     The Tea Party Awaits")
+	for _, file := range files {
+		fmt.Fprintln(w, file.Name())
+	}
+}
+
 
 func main() {
-	var err error
-	var files []string
-	//var interfaces, interfaceList []string
-	//var adapterNum uint32 = 0
+	
 
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/eatme", eatMeHandler)
+	http.HandleFunc("/drinkme", drinkMeHandler)
 
 	http.ListenAndServe(":8090", nil)
 
-	wait(3)
 
-
-MainMenu:
-	fmt.Println("Choose One")
-	fmt.Println("1. Eat Me")
-	fmt.Println("2. Drink Me")
-	fmt.Println("3. Wake Up")
-	res := StringPrompt("One or Two, What shall I do?")
-	if res == "1" {
-		goto EatMe
-	} else if res == "2" {
-		goto DrinkMe
-	} else if res == "3" {
-		Cls()
-		fmt.Println("Goodbye Friend!")
-		time.Sleep(3 * time.Second)
-		goto Exit
-	} else {
-		fmt.Println("1, 2 or 3, it's all the same to me...")
-		time.Sleep(3 * time.Second)
-		goto MainMenu
-	}
-
-EatMe:
-	Cls()
-		files, err = SleepTalk.Map("github.com/Chees3loaf/Pokedex/Snorlax/Backpack/EatMe")
-	if err != nil {
-    	fmt.Println("Error:", err)
-	} else {
-    	fmt.Println("The Finest of Tavern Foods")
-    	for _, filename := range files {
-        	fmt.Println(filename)
-    	}
-	}
-
-
-
-DrinkMe:
-	Cls()
-		files, err = SleepTalk.Map("github.com/Chees3loaf/Pokedex/Snorlax/Backpack/DrinkMe")
-	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Println("A Very Marry Un-Birthday to You!")
-		fmt.Println("    The Tea Party Awaits")
-		for _, filename := range files {
-			fmt.Println(filename)
-		}
-	}
-
-
-	
-
-Exit:
-	os.Exit(0)
 }
--- Snorlax/go.mod
+++ Snorlax/go.mod
@@ -1,3 +1,5 @@
 module github.com/Chees3loaf/Pokedex/Snorlax
 
 go 1.21.1
+
+require fyne.io/fyne/v2 v2.4.0 // indirect
